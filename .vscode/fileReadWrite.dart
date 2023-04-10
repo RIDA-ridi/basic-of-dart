db3e5651a840279df04a418ccffc5652ad3a9f8b
rida <ridich99999@gmail.com>
2023-04-10 23:33:31 +0500

    commit

---
+++ .vscode/fileReadWrite.dart
@@ -0,0 +1,9 @@
+import "dart:async";
+import "dart:io";
+
+void main() {
+  File f = new File('D:\\File Explorer.txt');
+  f.writeAsStringSync("hi how's going everyone");
+  (f.readAsStringSync());
+//f.deleteSync();
+}
---
+++ .vscode/listOfFile.dart
@@ -0,0 +1,14 @@
+import "dart:async";
+import "dart:io";
+
+void main() {
+  /*File f = new File('D:\\text.txt');
+  f.writeAsStringSync("hello world");
+  f.readAsStringSync();*/
+  Directory dir = new Directory('D:\\Amazon Full');
+  List<FileSystemEntity> files = dir.listSync(recursive: true);
+  for (FileSystemEntity file in files) {
+    FileStat f1 = file.statSync();
+    print(f1.toString());
+  }
+}
