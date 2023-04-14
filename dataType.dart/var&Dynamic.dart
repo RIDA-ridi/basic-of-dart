016ccd38cb15d30a348779488b34ba6d2d7345a1
rida <ridich99999@gmail.com>
2023-04-14 11:33:27 +0500

    commit

---
+++ dataType.dart/var&Dynamic.dart
@@ -0,0 +1,15 @@
+void main() {
+  var rolNo = 66; //data assign in var for the 1st time then var only
+  rolNo = 88; // store that kind of data
+  print(rolNo);
+
+  var test; //dynamic type of var
+  test = 44;
+  test = 10.0;
+  test = "hello";
+  print(test);
+  dynamic test1;
+  test1 = "testhello";
+  test1 = 77;
+  print(test1);
+}
