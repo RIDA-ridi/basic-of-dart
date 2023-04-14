3564997770bba7be743fc5770ab9d63c08c97850
rida <ridich99999@gmail.com>
2023-04-14 11:11:07 +0500

    commit

--- dataType.dart/hashMap.dart
+++ dataType.dart/hashMap.dart
@@ -6,7 +6,7 @@ void main() {
     fruits["guava"] = "green";
     fruits["stabery"] = "red";
   }
-  fruits.update("apple", (value) => "yellow");
+  fruits.update("apple", (value) => "yellow"); //for update the key value
 //  print(fruits);
   for (var key in fruits.keys) //print all the keys
   {
@@ -21,4 +21,11 @@ void main() {
 
   fruits.forEach((key, value) => print({"key:${key} values is :${value}"}));
 //use of for each loop
+
+  var map1 = Map<int, String>(); //third way to declare maps
+  {
+    map1[2] = "car";
+    map1[3] = "bus";
+  }
+  print(map1);
 }
