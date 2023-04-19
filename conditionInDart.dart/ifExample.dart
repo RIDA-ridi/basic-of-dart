ea7625e3a0d7390e50a331d1298eebf9bd5ee5ce
rida <ridich99999@gmail.com>
2023-04-19 12:56:58 +0500

    commit

--- conditionInDart.dart/ifExample.dart
+++
@@ -1,8 +0,0 @@
-import "dart:io";
-
-void main() {
-  print("enter your name");
-  String username = stdin.readLineSync()!;
-  if (username == "mike" || username == "alice") ;
-  print("hello ${username}");
-}
